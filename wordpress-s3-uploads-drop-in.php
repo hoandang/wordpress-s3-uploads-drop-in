40579875b0c96a50b416bd9e5db1ac749abab80a
Hoan Dang <danghuyhoan@gmail.com>
2019-07-13 02:51:41 +1000

    Remove autoload in composer

--- wordpress-s3-uploads-drop-in.php
+++ wordpress-s3-uploads-drop-in.php
@@ -1,7 +1,31 @@
 <?php
 /*
   Plugin Name: S3 Uploads DropIn
-  Version: 1.0
+  Version: 1.3
 */
 
-die('test');
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action('plugins_loaded', [S3Uploads::get_instance(), 'plugin_setup']);
+
+class S3Uploads 
+{
+  protected function __construct() { }
+  protected function __wakeup() { }
+  protected function __clone() { }
+
+  public static function get_instance()
+  {
+    static $instance = null;
+    if ($instance === null) {
+      $instance = new static();
+    }
+    return $instance;
+  }
+
+  public function plugin_setup()
+  {
+    add_filter('wp_handle_upload_prefilter', function($file) {
+    });
+  }
+}
